90d3adcfd473e471e500d0b22a73391f48e24275
abel <35218668+almabel01@users.noreply.github.com>
2025-11-20 11:30:40 +0000

    key overrides

--- Geed3/keymap.c
+++ Geed3/keymap.c
@@ -1271,7 +1271,7 @@ void leader_end_user(void) {
 const key_override_t delete_key_override = 
     ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
 
-const key_override_t **key_overrides = (const key_override_t *[]){
+const key_override_t *key_overrides[] = {
 	&delete_key_override,
 	NULL
 };
