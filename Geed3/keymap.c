e7215ee3f253c4f0eba8582bf04bd8455b27c56b
abel <35218668+almabel01@users.noreply.github.com>
2025-11-20 11:16:54 +0000

    shift+backspace => del

--- Geed3/keymap.c
+++ Geed3/keymap.c
@@ -1267,3 +1267,11 @@ void leader_end_user(void) {
     STATUS_LED_4(false);
 
 }
+
+const key_override_t delete_key_override = 
+    ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
+
+const key_override_t **key_overrides = (const key_override_t *[]){
+	&delete_key_override,
+	NULL
+};
