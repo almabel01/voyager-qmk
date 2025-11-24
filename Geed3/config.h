92df05b751148373f0c9f8e87c96e10fe7aae466
abel <35218668+almabel01@users.noreply.github.com>
2025-11-24 15:05:32 +0000

    merge

--- Geed3/config.h
+++ Geed3/config.h
@@ -8,7 +8,7 @@
 #define RGB_MATRIX_TIMEOUT 300000
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
-#define SERIAL_NUMBER "Geed3/YnVrGb"
+#define SERIAL_NUMBER "Geed3/Dzw9z0"
 #define LAYER_STATE_16BIT
 #define COMBO_COUNT 9
 
